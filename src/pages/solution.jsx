c9313bb63c38cced23ddd6da5f25822650f41982
naufalarzh <zhfrn81@gmail.com>
2025-11-12 12:28:23 +0700

    first commit

---
+++ src/pages/solution.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const solution = () => {
+  return (
+    <div>
+      <nav></nav>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+};
+
+export default solution;
